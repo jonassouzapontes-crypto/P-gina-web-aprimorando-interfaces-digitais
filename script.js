7a81349f70bced25613131ccd053845bfbdfc9ae
jonassouzapontes-crypto <jonas.souza.pontes@escola.pr.gov.br>
2026-08-27 19:28:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -82,5 +82,5 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    console.log('🚀 Site RasinformaticA carregado com sucesso!');
+    console.log('🚀 Site jonas carregado com sucesso!');
 });
